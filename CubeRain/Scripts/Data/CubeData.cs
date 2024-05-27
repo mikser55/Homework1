9663f5ada2f97041acd62daa4ee8c3e63f94c69e
Vladislav <omega4545@yandex.ru>
2024-05-27 12:45:12 +0700

    1

---
+++ CubeRain/Scripts/Data/CubeData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CubeData", menuName = "Data/CubeData")]
+public class CubeData : ScriptableObject
+{
+    [field: SerializeField] public int MinCubeLiveTime { get; private set; } = 2; 
+    [field: SerializeField] public int MaxCubeLiveTime { get; private set; } = 5; 
+}
