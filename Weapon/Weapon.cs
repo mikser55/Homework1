a708ef6d1174a068c525451b3099d258cd241ae1
Vladislav <omega4545@yandex.ru>
2024-08-04 04:01:22 +0700

    1

--- Weapon/Weapon.cs
+++ Weapon/Weapon.cs
@@ -1,18 +1,41 @@
-﻿class Weapon
+﻿using System;
+
+class Weapon
 {
-    private int _damage;
+    private readonly int _damage;
     private int _bullets;
 
     public Weapon(int damage, int bullets)
     {
-        _damage = damage;
-        _bullets = bullets;
+        if (damage > 0 && bullets > 0)
+        {
+            _damage = damage;
+            _bullets = bullets;
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException();
+        }
     }
 
     public void Fire(Player player)
     {
-        player.TakeDamage(_damage);
-        _bullets--;
+        if (player != null)
+        {
+            if (_bullets > 0)
+            {
+                player.TakeDamage(_damage);
+                _bullets--;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+        else
+        {
+            throw new ArgumentNullException();
+        }
     }
 }
 
@@ -29,20 +52,30 @@ class Player
             if (_health < 0)
                 _health = 0;
         }
+        else
+        {
+            throw new ArgumentOutOfRangeException();
+        }
     }
 }
 
 class Bot
 {
-    private Weapon _weapon;
+    private readonly Weapon _weapon;
 
     public Bot(Weapon weapon)
     {
-        _weapon = weapon;
+        if (_weapon != null)
+            _weapon = weapon;
+        else
+            throw new ArgumentNullException();
     }
 
     public void OnSeePlayer(Player player)
     {
-        _weapon.Fire(player);
+        if (player != null)
+            _weapon.Fire(player);
+        else
+            throw new ArgumentNullException();
     }
 }
