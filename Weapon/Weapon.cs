4cfd497e3eeef359354265e43e14c3d270af417b
Vladislav <omega4545@yandex.ru>
2024-08-03 09:35:42 +0700

    1

---
+++ Weapon/Weapon.cs
@@ -0,0 +1,48 @@
+﻿class Weapon
+{
+    private int _damage;
+    private int _bullets;
+
+    public Weapon(int damage, int bullets)
+    {
+        _damage = damage;
+        _bullets = bullets;
+    }
+
+    public void Fire(Player player)
+    {
+        player.TakeDamage(_damage);
+        _bullets--;
+    }
+}
+
+class Player
+{
+    private int _health;
+
+    public void TakeDamage(int damage)
+    {
+        if (damage > 0)
+        {
+            _health -= damage;
+
+            if (_health < 0)
+                _health = 0;
+        }
+    }
+}
+
+class Bot
+{
+    private Weapon _weapon;
+
+    public Bot(Weapon weapon)
+    {
+        _weapon = weapon;
+    }
+
+    public void OnSeePlayer(Player player)
+    {
+        _weapon.Fire(player);
+    }
+}
